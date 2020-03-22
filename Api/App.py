d87650f2618022e96f14bc68415b453520a1b4bf
mosorio <mijailosorioh@gmail.com>
2020-03-22 14:03:12 -0500

    post data

--- Api/App.py
+++ Api/App.py
@@ -1,75 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mysqldb import MySQL
+from flask import Flask, request
+from flask import render_template
 
-# initializations
-app = Flask(__name__)
+app=Flask(__name__,template_folder='templates')
 
-# Mysql Connection
-app.config['MYSQL_HOST'] = '192.168.100.51' 
-app.config['MYSQL_USER'] = 'Qatest'
-app.config['MYSQL_PASSWORD'] = 'Quito.2019'
-app.config['MYSQL_DB'] = 'COVID19'
-mysql = MySQL(app)
-
-# settings
-app.secret_key = "mysecretkey"
-
-# routes
 @app.route('/')
-def Index():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM contacts')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('index.html', contacts = data)
+def home():
+    return 'Api rest Covid-19'
 
-@app.route('/add_contact', methods=['POST'])
-def add_contact():
+@app.route('/Registro', methods=['POST'])
+def Registro():
     if request.method == 'POST':
-        fullname = request.form['fullname']
-        phone = request.form['phone']
-        email = request.form['email']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO contacts (fullname, phone, email) VALUES (%s,%s,%s)", (fullname, phone, email))
-        mysql.connection.commit()
-        flash('Contact Added successfully')
-        return redirect(url_for('Index'))
+        Pais = request.form['Pais']
+        Region = request.form['Provincia']
+        City = request.form['Ciudad']
+        Sector = request.form['Sector']
+        TotalPersonas = request.form['TotalPersonas']
+        TotalSalidas = request.form['TotalSalidas']
+        intervaloSalida = request.form['IntervaloSalida']
+        print (Pais)
+        print (Region)
+        print (City)
+        #x = Pais + Region + City +Sector + TotalPersonas + TotalSalidas + intervaloSalida 
+    return render_template('Validar-Registro.html')
 
-@app.route('/edit/<id>', methods = ['POST', 'GET'])
-def get_contact(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM contacts WHERE id = %s', (id))
-    data = cur.fetchall()
-    cur.close()
-    print(data[0])
-    return render_template('edit-contact.html', contact = data[0])
-
-@app.route('/update/<id>', methods=['POST'])
-def update_contact(id):
-    if request.method == 'POST':
-        fullname = request.form['fullname']
-        phone = request.form['phone']
-        email = request.form['email']
-        cur = mysql.connection.cursor()
-        cur.execute("""
-            UPDATE contacts
-            SET fullname = %s,
-                email = %s,
-                phone = %s
-            WHERE id = %s
-        """, (fullname, email, phone, id))
-        flash('Contact Updated Successfully')
-        mysql.connection.commit()
-        return redirect(url_for('Index'))
-
-@app.route('/delete/<string:id>', methods = ['POST','GET'])
-def delete_contact(id):
-    cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
-    mysql.connection.commit()
-    flash('Contact Removed Successfully')
-    return redirect(url_for('Index'))
-
-# starting the app
-if __name__ == "__main__":
-    app.run(port=5080, debug=True)
+if __name__ == '__main__':
+    #app.run( )
+    app.run(port=5050, debug=True)
--- Api/test.py
+++ Api/test.py
@@ -1,6 +1,14 @@
 from flask import Flask, request
+from flask import render_template
+# BD
+import time
+import datetime 
+from datetime import date
+from datetime import datetime
+import pymysql
+from flask_cors import CORS
 
-app = Flask(__name__)
+app=Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def home():
@@ -12,11 +20,51 @@ def Registro():
         Pais = request.form['Pais']
         Region = request.form['Provincia']
         City = request.form['Ciudad']
+        Sector = request.form['Sector']
+        TotalPersonas = request.form['TotalPersonas']
+        TotalSalidas = request.form['TotalSalidas']
+        intervaloSalida = request.form['IntervaloSalida']
+        fechapeticion = date.today()
+        now = datetime.now()
+        timeget = now.time()
+        print ("//////////////////////////////////////////////////")
+        print (fechapeticion)
+        print (now)
+        print(timeget)
         print (Pais)
         print (Region)
         print (City)
-        x = Pais + Region + City
-    return x
+        print (Sector)
+        print (TotalPersonas)
+        print (TotalSalidas)
+        print (intervaloSalida)
+        # IP del Servidor
+        # user_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+        #Ip client
+        user_ip = request.environ["REMOTE_ADDR"]
+
+        #x = Pais + Region + City +Sector + TotalPersonas + TotalSalidas + intervaloSalida 
+    # Connect to the database
+    connection = pymysql.connect(host='192.168.100.51',
+                                user='Qatest',
+                                password='Quito.2019',
+                                db='COVID19',
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+    # Create a new record
+                            
+            sql = "INSERT INTO `Data` (`Id_Pais`, `Id_Region`, `Id_City`, `Id_Sector`, `Total_Personas_Casa`, `Total_personas_Salida`, `Time_Aprox_Salida`, `IP`, `Date`, `Hora`) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (Pais, Region, City, Sector, TotalPersonas, TotalSalidas, intervaloSalida, user_ip, fechapeticion, timeget))
+            
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+            connection.commit()
+
+    finally:
+        connection.close()
+    return render_template('Validar-Registro.html')
 
 if __name__ == '__main__':
     #app.run( )
