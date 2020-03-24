6dc0c3c58906bb04e85defd375f666c3a2c265b5
mosorio <mijailosorioh@gmail.com>
2020-03-23 21:06:17 -0500

    pie leyensa

--- Api/App.py
+++ Api/App.py
@@ -276,8 +276,8 @@ def PieChart():
                 {"id":"","label":"Slices","pattern":"","type":"number"}
                 ],
                 "rows": [
-                {"c":[{"v":"Male1","f":"Male1A"},{"v":male,"f":male}]},
-                {"c":[{"v":"Female2","f":"Female2A"},{"v":Female,"f":Female}]}
+                {"c":[{"v":"Male1","f":"Personas en Casa"},{"v":male,"f":male}]},
+                {"c":[{"v":"Female2","f":"Personas que Salieron"},{"v":Female,"f":Female}]}
                 ]
                 })
     finally:
