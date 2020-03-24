eb400c2fa5850bb1a44965c6dfb0ed09e0989b5d
mosorio <mijailosorioh@gmail.com>
2020-03-23 21:01:47 -0500

    leyenda

--- Api/App.py
+++ Api/App.py
@@ -276,8 +276,8 @@ def PieChart():
                 {"id":"","label":"Slices","pattern":"","type":"number"}
                 ],
                 "rows": [
-                {"c":[{"v":"Male","f":"Male"},{"v":male,"f":male}]},
-                {"c":[{"v":"Female","f":"Female"},{"v":Female,"f":Female}]}
+                {"c":[{"v":"Male1","f":"Male"},{"v":male,"f":male}]},
+                {"c":[{"v":"Female1","f":"Female"},{"v":Female,"f":Female}]}
                 ]
                 })
     finally:
